0a92b4e3dbbaa15cc1c3223023bd2dae4daae6ef
Daniel Martinez <eduardodaniel2@gmail.com>
2013-07-19 10:11:13 -0500

    Adds lazy loading of RecordRoo module classes

--- lib/record_roo.rb
+++ lib/record_roo.rb
@@ -1,53 +1,12 @@
 require "csv"
 require "roo"
-require "record_roo/version"
 
 module RecordRoo
-	class DataSource
-	  def initialize filepath
-	    @filepath = filepath
-	  end
+  autoload :VERSION,    './record_roo/version'
 
-	  def parse
-	    @adapter = Class.instance_eval adapter
-	    @adapter.parse @filepath
-	  end
+  autoload :DataSource, './record_roo/data_source'
 
-	  def model_generator model_with_attributes
-	    parse.drop(1).map do |row|
-	      model = model_with_attributes[:model]
-	      object = Hash[model_with_attributes[:attributes].map.with_index(0) do |attr, index|
-	        [attr, row[index]]
-	      end]
-	      model.new(object)
-	    end
-	  end
-
-	  private
-	  def raw_extension
-	    File.extname @filepath
-	  end
-
-	  def adapter
-	    raw_extension.match(/\w+/).to_s.capitalize
-	  end
-	end
-
-	class Xlsx
-	  def self.parse filepath
-	    CSV.parse Roo::Excelx.new(filepath).to_csv
-	  end
-	end
-
-	class Xls
-	  def self.parse filepath
-	    CSV.parse Roo::Excel.new(filepath).to_csv
-	  end
-	end
-
-	class Csv
-	  def self.parse filepath
-	    CSV.read filepath
-	  end
-	end
+  autoload :Csv,        './record_roo/formats/csv'
+  autoload :Xlsx,       './record_roo/formats/xlsx'
+  autoload :Xls,        './record_roo/formats/xls'
 end
